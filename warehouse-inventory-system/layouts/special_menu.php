ccf76aefc9ade14af5b22565b47ed2e8a0b849fb
MOOREZHANG64 <moorezhang64@gmail.com>
2018-11-27 17:51:18 +0800

    edit special user role access right

--- warehouse-inventory-system/layouts/special_menu.php
+++ warehouse-inventory-system/layouts/special_menu.php
@@ -1,10 +1,12 @@
 <ul>
+<!--
   <li>
     <a href="home.php">
       <i class="glyphicon glyphicon-home"></i>
       <span>Dashboard</span>
     </a>
   </li>
+-->
   <li>
     <a href="categorie.php" >
       <i class="glyphicon glyphicon-indent-left"></i>
